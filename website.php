f4bf5e750f7d0a779f3eee8b328e0bcb8e7458e1
binkyfish <90187487+binkyfish@users.noreply.github.com>
2021-09-07 14:29:39 +0200

    s

--- website.php
+++ website.php
@@ -1,1 +1,23 @@
 <?php include 'header.php';?>
+<html lang="nl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Suntours</title>
+  </head>
+  <body>
+    <h1>Home Page</h1>
+
+    <div class="card border-dark mb-3" style="max-width: 18rem;">
+      <a href="#">
+        <img src="https://www.vakantiediscounter.nl/tools/content/images/NL/319/520.jpg" class="card-img-top" alt="..."></a>
+        <div class="card-footer bg-transparent border-dark">een mooie plek</div>
+      </div>
+      <div class="card border-dark mb-3" style="max-width: 18rem;">
+        <a href="#">
+          <img src="https://www.vakantiediscounter.nl/tools/content/images/NL/319/520.jpg" class="card-img-top" alt="..."></a>
+          <div class="card-footer bg-transparent border-dark">een mooie plek</div>
+        </div>
+  </body>
+</html>
+
+<?php include 'footer.php';?>
