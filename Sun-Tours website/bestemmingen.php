cf3a431a1e699542f2f13f6d5a0e0e0819a24cdd
Delletje <delon.poels@student.gildeopleidingen.nl>
2021-09-14 13:59:49 +0200

    boeken pagina maken

--- Sun-Tours website/bestemmingen.php
+++ Sun-Tours website/bestemmingen.php
@@ -2,10 +2,11 @@
 
 <?php
 
-    $sql = "SELECT `Locatie`,`Prijs`,`Plaatje` FROM `bestemming`";
+    $sql = "SELECT `ID`, `Locatie`,`Prijs`,`Plaatje` FROM `bestemming`";
     $result = mysqli_query($conn, $sql);
     while($fetch = mysqli_fetch_assoc($result)){
     $Bestemmingen[] = array(
+      'ID' => $fetch['ID'],
       'Locatie' => $fetch['Locatie'],
       'Prijs' => $fetch['Prijs'],
       'Plaatje' => $fetch['Plaatje']
@@ -21,17 +22,19 @@
           <link  rel="stylesheet" href="test.css" type ="text/css"/>
        </head>
        <body>
-          <?php //for ($i=0; $i < count($Bestemmingen); $i++) {
+         <?php //for ($i=0; $i < count($Bestemmingen); $i++) {
             foreach ($Bestemmingen as $bestemming) {
              ?>
-            <div class="card" id="bestemmingen">
-             <?php  echo '<img src="data:image/png;base64,'.base64_encode($bestemming['Plaatje']).'"/>'; ?>
-             <div class="card-body">
-                <h5 class="card-title"><?php echo $bestemming['Locatie'] ?></h5>
-                <p class="card-text"><?php echo $bestemming['Prijs'] . " " . "Euro p.p." ?></p>
-             </div>
-          </div>
-          <?php }
+            <a href="boeken.php?id=<?php echo $bestemming['ID']; ?>">
+               <div class="card" id="bestemmingen">
+               <?php  echo '<img src="data:image/png;base64,'.base64_encode($bestemming['Plaatje']).'"/>'; ?>
+                  <div class="card-body">
+                     <h5 class="card-title"><?php echo $bestemming['Locatie'] ?></h5>
+                     <p class="card-text"><?php echo $bestemming['Prijs'] . " " . "Euro p.p." ?></p>
+                  </div>
+               </div>
+            </a>
+         <?php }
         //}  ?>
        </body>
     </html>
--- Sun-Tours website/boeken.php
+++ Sun-Tours website/boeken.php
@@ -1,9 +1,14 @@
-<?php
+<?php include_once 'header.php';
+ob_start();
+include 'bestemmingen.php';
+ob_end_clean();
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "sunproject";
 
+$id = $_GET['id'];
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -11,17 +16,9 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT KlantID, Voornaam, Achternaam FROM klant";
-$result = $conn->query($sql);
+$query = "SELECT ";
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["KlantID"]. " - Name: ". $row["Voornaam"]. " " . $row["Achternaam"] . "<br>";
-    }
-} else {
-    echo "0 results";
-}
+// echo '<img src="data:image/png;base64,'.base64_encode($bestemming['Plaatje']).'"/>';
 
 $conn->close();
 ?>
@@ -32,85 +29,28 @@ $conn->close();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link  rel="stylesheet" href="test.css" type ="text/css"/>
     <title>Boeken</title>
-    <style>
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  width: 30%;
-  margin-left: 20px;
-  margin-top: 20px;
-  float: left;
-}
-
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
-
-.container {
-  padding: 2px 16px;
-  background-color: lightblue;
-}
-img {
-    float: left;
-    width:  100px;
-    height: 200px;
-    object-fit: cover;
-}
-#filter {
-    height: 100px;
-    width: 20%;
-    border: 1px solid black;
-    float: left;
-}
-#bestemmingen {
-    position: relative;
-    border: 1px solid black;
-    float: left;
-    width: 79.7%;
-    height: auto;
-}
-</style>
 </head>
 <body>
-    <div id="filter"></div>
-    <div id="bestemmingen">
-        <a href="">
-            <div class="card">
-                <img src="Mallorca.jpg" alt="Bestemming" style="width:100%">
-                <div class="container">
-                    <h4><b>Mallorca</b></h4> 
-                    <p>Prijs</p> 
-                </div>
-            </div>
-        </a>
-        <a href="">
-            <div class="card">
-                <img src="Turkije.jpg" alt="Bestemming" style="width:100%">
-                <div class="container">
-                    <h4><b>Turkije</b></h4> 
-                    <p>Prijs</p> 
-                </div>
-            </div>
-        </a>
-        <a href="">
-            <div class="card">
-                <img src="Turkije.jpg" alt="Bestemming" style="width:100%">
-                <div class="container">
-                    <h4><b>Turkije</b></h4> 
-                    <p>Prijs</p> 
-                </div>
-            </div>
-        </a>
-        <a href="">
-            <div class="card">
-                <img src="Turkije.jpg" alt="Bestemming" style="width:100%">
-                <div class="container">
-                    <h4><b>Turkije</b></h4> 
-                    <p>Prijs</p> 
-                </div>
-            </div>
-        </a>
-    </div>
+    <form id="boekform" action="boeken.php" method="post">
+        <?php echo $bestemming['Locatie']; ?><br><br>
+
+        <div class="form-group input-group">
+            <input name="personen" class="form-control" placeholder="Personen" type="text" required>
+        </div>
+        <div class="form-group input-group">
+            <label>Vertrekdatum</label><input name="vertrekdatum" class="form-control" placeholder="MM/DD/YYYY" type="date" required>
+        </div>
+        <div class="form-group input-group">
+            <input name="duur" class="form-control" placeholder="Duur" type="text" required><label>Dagen</label>
+        </div>
+        <br><label>Prijs</label><?php echo " " . $id; ?><br>
+
+        <input type="submit" value="Koop">
+    </form>
+    <content><?php echo '<img src="data:image/png;base64,'.base64_encode($bestemming['Plaatje']).'"/>';?></content>
 </body>
 </html>
+
+<?php include 'footer.php';?>
