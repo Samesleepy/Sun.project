791a7beb0be3ac6653e5fb7f4c3c1cde5afb0404
Delletje <delon.poels@student.gildeopleidingen.nl>
2021-09-16 09:03:35 +0200

    Update boeken.php

--- Sun-Tours website/boeken.php
+++ Sun-Tours website/boeken.php
@@ -1,7 +1,5 @@
 <?php include_once 'header.php';
-
 $id = $_GET['id'];
-
 
 $query = "SELECT `Locatie`, `Prijs`, `Plaatje` FROM `bestemming` WHERE `ID` = '".$id."'";
     $result = mysqli_query($conn, $query);
@@ -12,7 +10,25 @@ $query = "SELECT `Locatie`, `Prijs`, `Plaatje` FROM `bestemming` WHERE `ID` = '"
       'Plaatje' => $fetch['Plaatje']
      );
     }
-
+$prijs = false;
+if(isset($_POST['personen'])){
+    $prijs = $_POST['personen'];
+}else{
+    $prijs = false;
+}
+if($prijs){
+    $prijs = intval($prijs) * intval($Bestemmingen['Prijs']);
+    ?><script>if(confirm("Druk op OK om te kopen voor <?php echo "€" . $prijs; ?>")){alert("Betaald!");window.location.href = "home.php"}</script><?php
+}
+// function updatePrijs($personen, $prijspp){
+//     echo "hallo";
+//     $resultaat = $personen * $prijspp;
+//     echo $resultaat;
+// }
+function updatePrijs(){
+    echo "hallo";
+    
+}
 $conn->close();
 ?>
 
@@ -26,11 +42,11 @@ $conn->close();
     <title>Boeken</title>
 </head>
 <body>
-    <form id="boekform" action="boeken.php" method="post">
+    <form id="boekform" action="boeken.php?id=<?php echo $id; ?>" method="post">
         <?php echo $Bestemmingen['Locatie']; ?><br><br>
 
         <div class="form-group input-group">
-            <input name="personen" class="form-control" placeholder="Personen" type="text" required>
+            <input name="personen" class="form-control" placeholder="Personen" type="text" onkeyup="updatePrijs()"  required> <!--this.value,$Bestemmingen['Prijs'])" required>-->
         </div>
         <div class="form-group input-group">
             <input name="vertrekdatum" class="form-control" placeholder="MM/DD/YYYY" type="date" required><label>Vertrekdatum</label>
@@ -38,12 +54,13 @@ $conn->close();
         <div class="form-group input-group">
             <input name="duur" class="form-control" placeholder="Duur" type="text" required><label>Dagen</label>
         </div>
-        <br><label>Prijs</label><?php echo " " . $Bestemmingen['Prijs']; ?><br>
+        <br><label>Prijs</label><?php echo " &euro;" . $Bestemmingen['Prijs'] . " "; ?><label>p.p.</label><br>
 
-        <input type="submit" value="Kopen" class="btn btn-primary btn-block">
+        <label>Totaal</label><?php if($prijs){echo " &euro;" . $prijs;}else{echo " &euro;0";} ?><br><br>
+
+        <input type="submit" value="Naar betalen" class="btn btn-primary btn-block">
     </form>
     <content><?php echo '<img src="data:image/png;base64,'.base64_encode($Bestemmingen['Plaatje']).'"/>';?></content>
 </body>
 </html>
-
 <?php include 'footer.php';?>
