069e2781248b00bced43823b46e56d7470a57541
Samesleepy <90187561+Samesleepy@users.noreply.github.com>
2021-09-21 12:14:07 +0200

    mini fix

--- Sun-Tours website/header.php
+++ Sun-Tours website/header.php
@@ -8,11 +8,10 @@ $databaseName = "sunproject";
 //connect to database
 try {
   $conn = new PDO("mysql:host=$host;dbname=$databaseName", $username, $password);
-  // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   //echo "Connected successfully";
 } catch(PDOException $e) {
-  //echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed";//: " . $e->getMessage();
 }
 
  ?>
@@ -85,7 +84,7 @@ try {
                        }
                       echo $_SESSION['Achternaam'];
                    }
-                       ?>
+                    ?>
                 </a>
               </li>
              </ul>
