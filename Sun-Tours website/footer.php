bccda2553dfc3ce6d5704f55dce2bfc7dbd442b5
Samesleepy <90187561+Samesleepy@users.noreply.github.com>
2021-09-21 12:14:39 +0200

    fix voor refresh bug

--- Sun-Tours website/footer.php
+++ Sun-Tours website/footer.php
@@ -10,4 +10,9 @@
   </body>
     <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script>
+      if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+      }
+</script>
 </html>
