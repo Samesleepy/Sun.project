1aa310ecc262eede8c474cbb8f970162c5909509
Samesleepy <90187561+Samesleepy@users.noreply.github.com>
2021-09-29 13:08:03 +0200

    Update bestemmingen.php

--- Sun-Tours website/bestemmingen.php
+++ Sun-Tours website/bestemmingen.php
@@ -5,6 +5,8 @@
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $Bestemmingen = $result;
 
+    //print_r($Bestemmingen);
+
     ?>
     <html>
        <head>
@@ -13,12 +15,23 @@
        <body>
          <?php
             foreach ($Bestemmingen as $bestemming) {
+              $stmt = $conn->prepare("SELECT `Score` FROM `review` WHERE `BestemmingID` = '".$bestemming['ID']."'");
+              $stmt->execute();
+              $results = array();
+              while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                array_push($results, $row['Score']);
+              }
+              $results = array_filter($results);
+              if(count($results)) {
+                  $score = array_sum($results)/count($results);
+              }
+
              ?>
             <a href="boeken.php?id=<?php echo $bestemming['ID']; ?>">
                <div class="card" id="bestemmingen">
                <?php  echo '<img src="data:image/png;base64,'.base64_encode($bestemming['Plaatje']).'"/>'; ?>
                   <div class="card-body">
-                     <h5 class="card-title"><?php echo $bestemming['Locatie'] ?></h5>
+                     <h5 class="card-title"><?php echo $bestemming['Locatie'] . " Score: " . $score ?></h5>
                      <p class="card-text"><?php echo $bestemming['Prijs'] . " " . "Euro p.p." ?></p>
                   </div>
                </div>
