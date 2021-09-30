f4e182b3720edc75abc88828ca7145b0f8d84006
Delletje <delon.poels@student.gildeopleidingen.nl>
2021-09-30 09:39:19 +0200

    covid en review code tabs goed gemaakt

--- Sun-Tours website/covid.php
+++ Sun-Tours website/covid.php
@@ -9,21 +9,20 @@
   </head>
   <body>
     <iframe src="https://public.domo.com/cards/bWxVg" width="35%" height="600" marginheight="0" marginwidth="0" frameborder="0"></iframe>
-    <div class="grp12 banner">
-      </div>
-       <div class="row pm txt-row">
-		     <div class="col">
-		         <h2>Alles wat je moet weten over je vakantie</h2>
-             <p>Van gezondheidsverklaringen en vervoersmaatregelen tot hygiënevoorschriften op de bestemming. Er bestaat heel veel informatie over Covid-19. Je ziet haast door de bomen het bos niet meer. Daarom hebben we alles voor je op een rij gezet. Waar moet je op letten tijdens het boeken? Met welke maatregelen moet je rekening houden tijdens je reis? En zijn er specifieke protocollen op je bestemming? Lees je goed in.</p>
-             <p>&nbsp;</p>
-             <h3>CoronaCheck-app en Digitaal Corona Certificaat (DCC)</h3>
-             <p>Vanaf 1 juli 2021 is het Digitale Corona Certificaat (DCC) beschikbaar in de CoronaCheck-app. Dit maakt het makkelijker om binnen Europa te reizen. Vanaf 8 augustus 2021 is de CoronaCheck-app ook te gebruiken bij <strong>terugkeer</strong> <strong>naar Nederland</strong> van <strong>buiten</strong> de EU. Ben je gevaccineerd, heb je een negatieve testuitslag of een bewijs van herstel dan kun je in de app een QR-code aanmaken die vervolgens voor vertrek gescand wordt. Daarnaast kan het zijn dat jouw vakantiebestemming nog aanvullende eisen heeft om het land binnen te komen. Controleer voor vertrek goed welke maatregelen voor jouw bestemming gelden. Wij hebben per bestemming die we aanbieden alle eisen en maatregelen op een rijtje gezet:</p>
-		      </div>
-      </div>
+    <div class="grp12 banner"></div>
+    <div class="row pm txt-row">
+      <div class="col">
+        <h2>Alles wat je moet weten over je vakantie</h2>
+          <p>Van gezondheidsverklaringen en vervoersmaatregelen tot hygiënevoorschriften op de bestemming. Er bestaat heel veel informatie over Covid-19. Je ziet haast door de bomen het bos niet meer. Daarom hebben we alles voor je op een rij gezet. Waar moet je op letten tijdens het boeken? Met welke maatregelen moet je rekening houden tijdens je reis? En zijn er specifieke protocollen op je bestemming? Lees je goed in.</p>
+          <p>&nbsp;</p>
+          <h3>CoronaCheck-app en Digitaal Corona Certificaat (DCC)</h3>
+          <p>Vanaf 1 juli 2021 is het Digitale Corona Certificaat (DCC) beschikbaar in de CoronaCheck-app. Dit maakt het makkelijker om binnen Europa te reizen. Vanaf 8 augustus 2021 is de CoronaCheck-app ook te gebruiken bij <strong>terugkeer</strong> <strong>naar Nederland</strong> van <strong>buiten</strong> de EU. Ben je gevaccineerd, heb je een negatieve testuitslag of een bewijs van herstel dan kun je in de app een QR-code aanmaken die vervolgens voor vertrek gescand wordt. Daarnaast kan het zijn dat jouw vakantiebestemming nog aanvullende eisen heeft om het land binnen te komen. Controleer voor vertrek goed welke maatregelen voor jouw bestemming gelden. Wij hebben per bestemming die we aanbieden alle eisen en maatregelen op een rijtje gezet:</p>
+        </div>
+    </div>
     <div>
-     <a href="https://www.nederlandwereldwijd.nl/documenten/vragen-en-antwoorden/welke-landen-hebben-welke-kleurcode" target="_blank">Landen kleurcode</a>
-     <a href="https://www.nederlandwereldwijd.nl/reizen/reisadviezen" target="_blank">reisadviezen</a>
-   </br></br></br></br></br>
+      <a href="https://www.nederlandwereldwijd.nl/documenten/vragen-en-antwoorden/welke-landen-hebben-welke-kleurcode" target="_blank">Landen kleurcode</a>
+      <a href="https://www.nederlandwereldwijd.nl/reizen/reisadviezen" target="_blank">reisadviezen</a>
+    </br></br></br></br></br>
   </body>
 </html>
 
--- Sun-Tours website/review.php
+++ Sun-Tours website/review.php
@@ -12,21 +12,19 @@
             <div class="jumbotron text-center">
                <h1>Schrijf een review!<h1>
             </div>
-
             <form method="post">
-              <select class="form-select form-select-sm" id="inputGroupSelect01" name="score" required>
+               <select class="form-select form-select-sm" id="inputGroupSelect01" name="score" required>
                   <option value="">Score: </option>
                   <option value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
                   <option value="4">4</option>
                   <option value="5">5</option>
-              </select>
+               </select>
                <div class="input-group">
-                 <span class="input-group-text">Uw review: </span>
-                 <textarea class="form-control"  name="review"></textarea>
-              </div>
-
+                  <span class="input-group-text">Uw review: </span>
+                  <textarea class="form-control"  name="review"></textarea>
+               </div>
                <div class="form-group">
                   <div class="text-center">
                      <button type="submit" name="submit" class="btn btn-primary btn-block">Verstuur</button>
@@ -34,21 +32,20 @@
                </div>
             </form>
             <?php
-
             if(isset($_POST['submit'])){
 
-              $voornaam = $_SESSION['Voornaam'];
-              $achternaam = $_SESSION['Achternaam'];
-              $tussenvoegsel = $_SESSION['Tussenvoegsel'];
-              $score = $_POST['score'];
-              $review = $_POST['review'];
-              $datum = date('Y/m/d');
+               $voornaam = $_SESSION['Voornaam'];
+               $achternaam = $_SESSION['Achternaam'];
+               $tussenvoegsel = $_SESSION['Tussenvoegsel'];
+               $score = $_POST['score'];
+               $review = $_POST['review'];
+               $datum = date('Y/m/d');
 
-              $sql = "INSERT INTO `review`(`BestemmingID`,`Voornaam`, `Achternaam`, `Tussenvoegsel`, `Score`, `Opmerking`,`Datum`) VALUES(?,?,?,?,?,?,?)";
+               $sql = "INSERT INTO `review`(`BestemmingID`,`Voornaam`, `Achternaam`, `Tussenvoegsel`, `Score`, `Opmerking`,`Datum`) VALUES(?,?,?,?,?,?,?)";
 
-              $stmt= $db->prepare($sql);
-              $stmt->execute([$id,$voornaam, $achternaam, $tussenvoegsel, $score, $review, $datum]);
-              unset($_POST['submit']);
+               $stmt= $db->prepare($sql);
+               $stmt->execute([$id,$voornaam, $achternaam, $tussenvoegsel, $score, $review, $datum]);
+               unset($_POST['submit']);
             }
 
             $stmt = $db->prepare("SELECT `Voornaam`, `Achternaam`,`Tussenvoegsel`,`Score`, `Opmerking`, `Datum` FROM `review` WHERE `BestemmingID` = '".$id."'");
@@ -58,21 +55,21 @@
 
             //print_r($Reviews);
 
-               foreach ($Reviews as $review) {
-             ?>
-                        <div class="card">
-                         <div class="card-body">
-                            <h5 class="card-title"><?php echo $review['Voornaam'] . " " ;
-                              if($review['Tussenvoegsel'] != ""){
-                                 echo " " . $review['Tussenvoegsel'] . " ";
-                              }
-                            echo $review['Achternaam']; ?></h5>
-                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $review['Datum']?></h6>
-                            <p class="card-text"><?php echo $review['Score'] ?></p>
-                            <p class="card-text"><?php echo $review['Opmerking'] ?></p>
+            foreach ($Reviews as $review) {
+            ?>
+            <div class="card">
+               <div class="card-body">
+                  <h5 class="card-title"><?php echo $review['Voornaam'] . " " ;
+                  if($review['Tussenvoegsel'] != ""){
+                     echo " " . $review['Tussenvoegsel'] . " ";
+                  }
+                  echo $review['Achternaam']; ?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?php echo $review['Datum']?></h6>
+                  <p class="card-text"><?php echo $review['Score'] ?></p>
+                  <p class="card-text"><?php echo $review['Opmerking'] ?></p>
 
-                         </div>
-                      </div>
+               </div>
+            </div>
          <?php } ?>
          </div>
       </div>
