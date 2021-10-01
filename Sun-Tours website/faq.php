cae3fa2e9b1f430fe5e636074f18a8d025232253
Samesleepy <90187561+Samesleepy@users.noreply.github.com>
2021-10-01 11:26:40 +0200

    faq beter gemaakt

--- Sun-Tours website/faq.php
+++ Sun-Tours website/faq.php
@@ -1,10 +1,9 @@
 <?php include_once 'header.php' ?>
 
-<!DOCTYPE html>
-
   <head>
     <meta charset="utf-8">
     <title>FAQ</title>
+        <link rel="stylesheet" href="test.css"/>
   </head>
   <body>
     <body>
@@ -15,18 +14,24 @@
       $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
       $FAQs = $result;
 
+      ?><div id="faqpage">
+      <h1>Welkom op de FAQ!</h1>
+        <h3>hier kunt u antwoorden voor veelgestelde vragen vinden.</h3><?php
        foreach ($FAQs as $FAQ) {
           ?>
-          <div class="card text-white bg-primary" id="faq">
+          <div id="faq" class="card border-primary" >
              <div class="card-body">
-                <h2 class="card-title"><?php echo $FAQ['Vraag-NL']?></h5>
+                <h2 class="card-header text-white bg-primary"><?php echo $FAQ['Vraag-NL']?></h5>
                 <h4 class="card-text"><?php echo $FAQ['Antwoord-NL']?></h4>
              </div>
           </div>
        </a>
     <?php } ?>
-    </body>
+    <h2>Is uw vraag nog niet beantwoord?</h2>
+      <form action='contact.php'>
+         <button type='submit' name='contact' class='btn btn-primary btn-block'>Terug naar contact</button>
+      </form>
+    </div>
   </body>
-
 
 <?php include_once 'footer.php' ?>
