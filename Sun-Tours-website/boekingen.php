da76aed34cfbb2a161603ccf499927952d24eccd
Delletje <delon.poels@student.gildeopleidingen.nl>
2021-10-05 13:57:11 +0200

    Update boekingen.php

--- Sun-Tours-website/boekingen.php
+++ Sun-Tours-website/boekingen.php
@@ -4,12 +4,6 @@ $db = $database->connection();
 
 $stmt = $db->query("SELECT `Land`,`Plaats`,`Prijs`,`Personen`,`Vertrekdatum`,`Duur` FROM `boeking` WHERE `KlantID` = '".$_SESSION['KlantID']."'");
 $Boekingen = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-
-
-
 ?>
     <body>
         <div class="container col-xl-10 col-xxl-8 px-4 py-5">
@@ -21,20 +15,27 @@ $Boekingen = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 $Bestemmingen = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                 ?>
-            <div class="card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-3">
-                        <?php echo '<img src="data:image/png;base64,'.base64_encode($Bestemmingen[0]['Plaatje']).'" class="img-fluid rounded-start"/>';?>
-                    </div>
-                    <div class="col-md-9">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $Boeking['Plaats'].", ".$Boeking['Land']; ?></h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted"><i class="fas fa-users"></i><?php echo " ".$Boeking['Personen']; ?></small></p>
+            <a href="#" style="text-decoration: none;color: black">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-3">
+                            <?php echo '<img src="data:image/png;base64,'.base64_encode($Bestemmingen[0]['Plaatje']).'" class="img-fluid rounded-start"/>';?>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $Boeking['Plaats'].", ".$Boeking['Land']; ?></h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                                <div style="float: right;">
+                                    <p class="card-text"><small class="text-muted"><i class="fas fa-users"></i><?php echo " ".$Boeking['Personen']; ?></small></p>
+                                    <p class="card-text"><small class="text-muted"><i class="fas fa-clock"></i><?php echo " ".$Boeking['Duur']." Dagen"; ?></small></p>
+                                    <p class="card-text"><small class="text-muted"><i class="fas fa-calendar-alt"></i></i><?php echo " ".$Boeking['Vertrekdatum']; ?></small></p>
+                                </div>
+                                <p class="card-text" style="margin-top: 6%;"><small class="text-muted">Boekingsdatum</small></p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </a>
             <?php } ?>
         </div>
     </body>
