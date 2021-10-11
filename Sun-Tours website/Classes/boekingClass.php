034175f152c5f9073aa45979d0a3346c8a34bc0c
Samesleepy <90187561+Samesleepy@users.noreply.github.com>
2021-10-11 09:02:07 +0200

    niks

--- Sun-Tours website/Classes/boekingClass.php
+++ Sun-Tours website/Classes/boekingClass.php
@@ -16,7 +16,7 @@ class Boeking
   function Boeken($database, $BestemmingID,$KlantID,$Land,$Plaats,$Personen,$Vertrekdatum,$Duur){
     $db = $database->connection();
     $query = $db->prepare("INSERT INTO `boeking` (`BestemmingID`, `KlantID`, `Land`,`Plaats`, `Prijs`, `Personen`, `Vertrekdatum`, `Duur`)
-    VALUES ($BestemmingID,$KlantID,$Land,$Plaats,$prijs,$Personen,$Vertrekdatum,$Duur)");
+    VALUES ('$BestemmingID','$KlantID','$Land','$Plaats','$prijs','$Personen','$Vertrekdatum','$Duur')");
     $query->execute();
   }
 }
