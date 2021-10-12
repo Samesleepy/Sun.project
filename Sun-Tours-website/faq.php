9fbe17af4a80c91046a59f4846a905634b756024
Samesleepy <90187561+Samesleepy@users.noreply.github.com>
2021-10-12 11:00:57 +0200

    faq fix?

--- Sun-Tours-website/faq.php
+++ Sun-Tours-website/faq.php
@@ -15,12 +15,18 @@
       ?>
       <h1 style="text-align: center;">Welkom op de FAQ!</h1>
       <h3 style="text-align: center;">hier kunt u antwoorden voor veelgestelde vragen vinden.</h3>
-
+      <div style="margin-top: 10px;" class="d-flex justify-content-center">
+      <div class="accordion accordion-flush" id="accordionFlushExample" style="width: 50%;border: 1px solid black">
+      <div class="accordion-header">
       <?php foreach ($FAQs as $key => $FAQ){
           $FAQ[$key] = new Faq($FAQ['QID'], $FAQ['Vraag-NL'], $FAQ['Antwoord-NL'], $FAQ['Vraag-EN'], $FAQ['Antwoord-EN']);
-          $FAQ->ShowFaq($key);
+          $FAQ[$key]->ShowFaq($key);
        }
         ?>
+      </div>
+        </div>
+      </div>
+    </div>
     <h2 style="text-align: center;">Is uw vraag nog niet beantwoord?</h2>
     <form action='contact.php' style="text-align: center;">
       <button type='submit' name='contact' class='btn btn-primary btn-block'>Terug naar contact</button>
