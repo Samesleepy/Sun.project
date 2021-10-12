a3c950a5cc246bd2ca808e4b6a8fba3ac572f5c0
Samesleepy <90187561+Samesleepy@users.noreply.github.com>
2021-10-12 12:08:48 +0200

    limiet fix

--- Sun-Tours-website/bestemmingen.php
+++ Sun-Tours-website/bestemmingen.php
@@ -1,19 +1,12 @@
 <?php include_once 'header.php';
 $db = $database->connection();
-$stmt = $db->prepare("SELECT bestemming.`ID`, bestemming.`Land`, bestemming.`Plaats`, `Type`, bestemming.`Beschrijving`, bestemming.`Prijs`,`Limiet`,`Plaatje`,
-AVG(`Score`), SUM(`Personen`)
-FROM `bestemming`
-LEFT JOIN review
-ON bestemming.ID = review.BestemmingID
-LEFT JOIN boeking
-ON bestemming.ID = boeking.BoekingID
-GROUP BY bestemming.ID;");
+$stmt = $db->prepare("SELECT bestemming.ID, bestemming.Land, bestemming.Plaats, bestemming.Type, bestemming.Prijs, bestemming.Beschrijving, bestemming.Limiet, bestemming.Plaatje, tableA.totalRes, tableB.avgRev from bestemming LEFT JOIN ( SELECT BestemmingID, SUM(personen) AS totalRes FROM boeking GROUP BY BestemmingID ) tableA ON bestemming.id = tableA.BestemmingID LEFT JOIN ( SELECT BestemmingID, AVG(score) AS avgRev FROM review GROUP BY BestemmingID ) tableB ON bestemming.id = tableB.BestemmingID;");
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $Bestemmingresult = $result;
 
 foreach ($Bestemmingresult as $bestemming) {
-  $Bestemmingen[] = new Bestemming($bestemming['ID'], $bestemming['Land'], $bestemming['Plaats'], $bestemming['Type'], $bestemming['Prijs'], $bestemming['Beschrijving'], $bestemming['Limiet'], $bestemming['Plaatje'], $bestemming['AVG(`Score`)'], $bestemming['SUM(`Personen`)']);
+  $Bestemmingen[] = new Bestemming($bestemming['ID'], $bestemming['Land'], $bestemming['Plaats'], $bestemming['Type'], $bestemming['Prijs'], $bestemming['Beschrijving'], $bestemming['Limiet'], $bestemming['Plaatje'], $bestemming['avgRev'], $bestemming['totalRes']);
  }
 
 ?>
