7c4fb9e9ee729dfd62e37b7d75988f418c0e02cf
JucksAble <80888980+JucksAble@users.noreply.github.com>
2021-10-13 11:58:12 +0200

    Update header.php

--- Sun-Tours-website/header.php
+++ Sun-Tours-website/header.php
@@ -45,18 +45,8 @@ if(isset($_POST['logout'])){
             <div class="collapse navbar-collapse" id="navbarNav">
                <ul class="navbar-nav">
                   <li class="nav-item">
-                  </li>
-                  <?php 
-                     if($User->voornaam == 'Sam'){      
-                  ?>
-                  <li class="nav-item">
-                     <a class="nav-link active" aria-current="page" href="https://code.visualstudio.com/download">Destinations</a>
-                  </li>
-                  <?php }else{ ?>
-                  <li class="nav-item">
                      <a class="nav-link active" aria-current="page" href="bestemmingen.php">Destinations</a>
                   </li>
-                  <?php } ?>
                   <li class="nav-item">
                      <a class="nav-link active" aria-current="page" href="covid.php">Covid-19 Measures</a>
                   </li>
