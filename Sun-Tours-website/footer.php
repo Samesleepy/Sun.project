01f2872907506b3573402dbb7e36a4fe2ba17a5f
JucksAble <80888980+JucksAble@users.noreply.github.com>
2021-11-02 09:54:37 +0100

    footer update

--- Sun-Tours-website/footer.php
+++ Sun-Tours-website/footer.php
@@ -1,12 +1,19 @@
 <body>
-  <!-- <div class="my-5"> asd</div> -->
-  <nav class="navbar fixed-bottom navbar-dark bg-dark">
-    <div class="footer-img">
-      <a class="navbar-brand" href="#">Sun-Tours</a>
-      <a href="https://twitter.com"><img src="Pics\Twitter-logo.png" class="twitter" height="40px" alt="Twitter" ></a>
-      <a href="https://www.instagram.com/"><img src="Pics\Instagram-logo.png" class="instagram" alt="Instagram" ></a>
-      <a href="https://www.facebook.com/"><img src="Pics\Facebook-logo.png" class="facebook" alt="Facebook" ></a>
-    </div>
+  <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-dark justify-content-md-center">
+      <div class="row" style="width: 1300px;">
+        <div class="col-4">
+          <a href="home.php" style="float: left;">
+              <img src="Pics/Sun-Tours-logo.png" class="sun-logo" alt="Suntours" style="filter: brightness(0) invert(1); width: 150px;">
+          </a>
+        </div>
+        <div class="col-8">
+          <div class="float-end">
+            <a href="https://twitter.com"><img src="Pics\Twitter-logo.png" class="twitter" height="40px" alt="Twitter" ></a>
+            <a href="https://www.instagram.com/"><img src="Pics\Instagram-logo.png" class="instagram" alt="Instagram" ></a>
+            <a href="https://www.facebook.com/"><img src="Pics\Facebook-logo.png" class="facebook" alt="Facebook" ></a>
+          </div>
+        </div>
+      </div>
   </nav>
 </body>
 <script>
